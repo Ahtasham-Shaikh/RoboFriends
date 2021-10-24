cc250258ee87d5c865bde1f0bf8855a328cc65a3
Ahtasham-Shaikh <ashaikh2u@gmail.com>
2021-10-24 14:27:11 +0530

    Initial

---
+++ src/components/CardList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Card from "./Card.js"
+
+const CardList = ({ robots }) => {
+  return (
+    robots.map((user,i) => {
+      return (
+        <Card
+        key={robots[i].id}
+        name={robots[i].name}
+        email={robots[i].email}
+        id={robots[i].id}/>
+      );
+    })
+  );
+}
+
+export default CardList
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import "./app.css"
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/SearchBox.js';
+import Scroll from "../components/Scroll.js";
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ""
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({
+      searchfield: event.target.value
+    })
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+      return response.json()
+    })
+    .then(users => {
+      this.setState({robots: users})
+    })
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return (robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()))
+    })
+    return (
+      <div className="tc">
+        <h1 className="f1" >ROBOFRIENDS</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots}/>
+        </Scroll>
+      </div>
+    )
+  }
+}
+
+
+export default App
